9359d7ae2c48d9b51f8b6dfab111f1acfb6f3316
PALLADIUM26 <pranithdutta26@gmail.com>
2024-07-21 16:05:33 +0530

    Initial build

---
+++ src/MainFrame.java
@@ -0,0 +1,21 @@
+//package FirstProject;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class MainFrame extends JFrame {
+    public void init() {
+        setTitle("Welcome");
+        setSize(450, 300);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
