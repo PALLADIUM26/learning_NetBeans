c85ac0b53a6e7dce5cdb967488591d57077bb9f4
PALLADIUM26 <pranithdutta26@gmail.com>
2024-07-28 10:32:00 +0530

    another build complete

--- src/MainFrame2.java
+++ src/MainFrame2.java
@@ -1,3 +1,12 @@
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Vector;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -25,26 +34,38 @@ public class MainFrame2 extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        javax.swing.JPanel jPanel1 = new javax.swing.JPanel();
+        javax.swing.JLabel jLabel1 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
-        jLabel2 = new javax.swing.JLabel();
+        javax.swing.JLabel jLabel2 = new javax.swing.JLabel();
         tfName = new javax.swing.JTextField();
         tfEmail = new javax.swing.JTextField();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
+        javax.swing.JLabel jLabel3 = new javax.swing.JLabel();
+        javax.swing.JLabel jLabel4 = new javax.swing.JLabel();
         tfPhone = new javax.swing.JTextField();
-        jLabel5 = new javax.swing.JLabel();
+        javax.swing.JLabel jLabel5 = new javax.swing.JLabel();
         tfAddress = new javax.swing.JTextField();
-        btnAdd = new javax.swing.JButton();
-        btnClr = new javax.swing.JButton();
-        btnDlt = new javax.swing.JButton();
+        javax.swing.JButton btnAdd = new javax.swing.JButton();
+        javax.swing.JButton btnClr = new javax.swing.JButton();
+        javax.swing.JButton btnDlt = new javax.swing.JButton();
+        javax.swing.JScrollPane jScrollPane1 = new javax.swing.JScrollPane();
+        tableClients = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Dashboard");
+        setMinimumSize(new java.awt.Dimension(897, 556));
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(java.awt.event.WindowEvent evt) {
+                formWindowClosing(evt);
+            }
+            public void windowOpened(java.awt.event.WindowEvent evt) {
+                formWindowOpened(evt);
+            }
+        });
 
         jPanel1.setBackground(new java.awt.Color(0, 204, 255));
 
-        jLabel1.setFont(new java.awt.Font("MV Boli", 0, 18)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("MV Boli", 0, 24)); // NOI18N
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/user.png"))); // NOI18N
         jLabel1.setText("List of Clients:");
@@ -52,33 +73,43 @@ public class MainFrame2 extends javax.swing.JFrame {
         jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "New Client", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Poor Richard", 0, 18))); // NOI18N
         jPanel2.setOpaque(false);
 
-        jLabel2.setFont(new java.awt.Font("Papyrus", 0, 12)); // NOI18N
+        jLabel2.setFont(new java.awt.Font("Papyrus", 3, 18)); // NOI18N
         jLabel2.setText("Name");
 
-        tfName.setFont(new java.awt.Font("Papyrus", 0, 12)); // NOI18N
+        tfName.setFont(new java.awt.Font("MS Gothic", 0, 14)); // NOI18N
 
-        tfEmail.setFont(new java.awt.Font("Papyrus", 0, 12)); // NOI18N
+        tfEmail.setFont(new java.awt.Font("MS Gothic", 0, 14)); // NOI18N
 
-        jLabel3.setFont(new java.awt.Font("Papyrus", 0, 12)); // NOI18N
+        jLabel3.setFont(new java.awt.Font("Papyrus", 3, 18)); // NOI18N
         jLabel3.setText("Email");
 
-        jLabel4.setFont(new java.awt.Font("Papyrus", 0, 12)); // NOI18N
+        jLabel4.setFont(new java.awt.Font("Papyrus", 3, 18)); // NOI18N
         jLabel4.setText("Phone");
 
-        tfPhone.setFont(new java.awt.Font("Papyrus", 0, 12)); // NOI18N
+        tfPhone.setFont(new java.awt.Font("MS Gothic", 0, 14)); // NOI18N
 
-        jLabel5.setFont(new java.awt.Font("Papyrus", 0, 12)); // NOI18N
+        jLabel5.setFont(new java.awt.Font("Papyrus", 3, 18)); // NOI18N
         jLabel5.setText("Address");
 
-        tfAddress.setFont(new java.awt.Font("Papyrus", 0, 12)); // NOI18N
+        tfAddress.setFont(new java.awt.Font("MS Gothic", 0, 14)); // NOI18N
 
-        btnAdd.setFont(new java.awt.Font("Papyrus", 0, 12)); // NOI18N
+        btnAdd.setFont(new java.awt.Font("Papyrus", 1, 18)); // NOI18N
         btnAdd.setIcon(new javax.swing.ImageIcon(getClass().getResource("/add.png"))); // NOI18N
         btnAdd.setText("Add");
+        btnAdd.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAddActionPerformed(evt);
+            }
+        });
 
-        btnClr.setFont(new java.awt.Font("Papyrus", 0, 12)); // NOI18N
+        btnClr.setFont(new java.awt.Font("Papyrus", 1, 18)); // NOI18N
         btnClr.setIcon(new javax.swing.ImageIcon(getClass().getResource("/clear.png"))); // NOI18N
         btnClr.setText("Clear");
+        btnClr.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnClrActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -125,15 +156,32 @@ public class MainFrame2 extends javax.swing.JFrame {
                 .addComponent(jLabel5)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(tfAddress, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 248, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 91, Short.MAX_VALUE)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnAdd)
                     .addComponent(btnClr))
                 .addGap(54, 54, 54))
         );
 
+        btnDlt.setFont(new java.awt.Font("Papyrus", 1, 18)); // NOI18N
         btnDlt.setIcon(new javax.swing.ImageIcon(getClass().getResource("/delete.png"))); // NOI18N
         btnDlt.setText("Delete");
+        btnDlt.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDltActionPerformed(evt);
+            }
+        });
+
+        tableClients.setFont(new java.awt.Font("Lucida Console", 0, 14)); // NOI18N
+        tableClients.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Name", "Email", "Phone", "Address"
+            }
+        ));
+        jScrollPane1.setViewportView(tableClients);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -141,12 +189,14 @@ public class MainFrame2 extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(14, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 867, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(btnDlt, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(btnDlt, javax.swing.GroupLayout.PREFERRED_SIZE, 133, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jScrollPane1))))
                 .addGap(16, 16, 16))
         );
         jPanel1Layout.setVerticalGroup(
@@ -156,10 +206,12 @@ public class MainFrame2 extends javax.swing.JFrame {
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(btnDlt)
-                        .addGap(0, 0, Short.MAX_VALUE))))
+                        .addGap(18, 18, 18)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 455, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(0, 15, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -175,6 +227,86 @@ public class MainFrame2 extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddActionPerformed
+        // TODO add your handling code here:
+        String name = tfName.getText();
+        String email = tfEmail.getText();
+        String phone = tfPhone.getText();
+        String address = tfAddress.getText();
+        
+        if(name.isEmpty() || email.isEmpty() || phone.isEmpty() || address.isEmpty()){
+            JOptionPane.showMessageDialog(this, "Please enter all fields", "Try Again", JOptionPane.ERROR_MESSAGE);
+        } else {
+            DefaultTableModel model = (DefaultTableModel) tableClients.getModel();
+            model.addRow(new Object[]{name, email, phone, address});
+            
+            tfName.setText("");
+            tfEmail.setText("");
+            tfPhone.setText("");
+            tfAddress.setText("");
+        }
+    }//GEN-LAST:event_btnAddActionPerformed
+
+    private void btnClrActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnClrActionPerformed
+        // TODO add your handling code here:
+        tfName.setText("");
+        tfEmail.setText("");
+        tfPhone.setText("");
+        tfAddress.setText("");
+    }//GEN-LAST:event_btnClrActionPerformed
+
+    private void btnDltActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDltActionPerformed
+        // TODO add your handling code here:
+        int row = tableClients.getSelectedRow();
+        
+        if(row < 0){
+            JOptionPane.showMessageDialog(this, "No row is selected, Select a row", "Select row", JOptionPane.ERROR_MESSAGE);
+        } else {
+            DefaultTableModel model = (DefaultTableModel) tableClients.getModel();
+            model.removeRow(row);
+        }
+        
+    }//GEN-LAST:event_btnDltActionPerformed
+
+    private void formWindowClosing(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosing
+        // TODO add your handling code here:
+        DefaultTableModel model = (DefaultTableModel) tableClients.getModel();
+        Vector<Vector> tableData = model.getDataVector();
+        
+        // Saving data in a file
+        try {
+            FileOutputStream file = new FileOutputStream("file.bin");
+            ObjectOutputStream output = new ObjectOutputStream(file);
+            
+            output.writeObject(tableData);
+            output.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }//GEN-LAST:event_formWindowClosing
+
+    private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
+        // TODO add your handling code here:
+        try {
+            FileInputStream file = new FileInputStream("file.bin");
+            ObjectInputStream input = new ObjectInputStream(file);
+            
+            Vector<Vector> tableData = (Vector<Vector>) input.readObject();
+            
+            input.close();
+            file.close();
+            
+            DefaultTableModel model = (DefaultTableModel) tableClients.getModel();
+            for(int i=0; i<tableData.size(); i++){
+                Vector row = tableData.get(i);
+                model.addRow(new Object[]{row.get(0), row.get(1), row.get(2), row.get(3)});
+            }
+            
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }//GEN-LAST:event_formWindowOpened
 
     /**
      * @param args the command line arguments
@@ -212,16 +344,8 @@ public class MainFrame2 extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnAdd;
-    private javax.swing.JButton btnClr;
-    private javax.swing.JButton btnDlt;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JTable tableClients;
     private javax.swing.JTextField tfAddress;
     private javax.swing.JTextField tfEmail;
     private javax.swing.JTextField tfName;
